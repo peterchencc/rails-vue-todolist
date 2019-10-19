a60a046f2ee08dcd367d95def195586fc55ddb40
peterchencc <peterchencc@gmail.com>
2019-10-19 21:53:58 +0800

    update vue with prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from '../todolists.vue'
 import VueResource from 'vue-resource'
-Vue.use(VueResource);
+Vue.use(VueResource)
 
 document.addEventListener('DOMContentLoaded', () => {
-  Vue.http.headers.common['X-CSRF-Token'] = document.getElementsByName('csrf-token')[0].getAttribute('content')
-  const el = "#todolists"
+  Vue.http.headers.common['X-CSRF-Token'] = document
+    .getElementsByName('csrf-token')[0]
+    .getAttribute('content')
+
+  const el = '#todolists'
   const app = new Vue({
     el,
     render: h => h(App)
--- app/javascript/todolists.vue
+++ app/javascript/todolists.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Todo Lists</h1>
 
-    <input type="text" v-model="todo" class="form-control" autofocus="true">
+    <input type="text" v-model="todo" class="form-control" autofocus="true" />
     <button @click="addTodo()" class="btn btn-primary" :disabled="!todo.length">Add Todo</button>
 
     <table>
@@ -14,12 +14,16 @@
       </thead>
 
       <tbody>
-        <tr v-for="todo in list" >
+        <tr v-bind:key="todo" v-for="todo in list">
           <td>{{ todo.id }}</td>
           <td>
-            <span @click="changeTodo(todo)" v-show="todo.editable"> {{ todo.item }}</span>
-            <input v-show="!todo.editable" v-model="todo.item" >
-            <button v-show="!todo.editable" @click="updateTodo(todo)" class="btn btn-primary">Update Todo</button>
+            <span @click="changeTodo(todo)" v-show="todo.editable">{{ todo.item }}</span>
+            <input v-show="!todo.editable" v-model="todo.item" />
+            <button
+              v-show="!todo.editable"
+              @click="updateTodo(todo)"
+              class="btn btn-primary"
+            >Update Todo</button>
           </td>
           <td>
             <button @click="deleteTodo(todo.id)" class="btn btn-primary">Delete Todo</button>
@@ -32,55 +36,55 @@
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
-      todo: '',
-      list: [
-        { id: 1, item: "Foo" },
-        { id: 2, item: "Bar" }
-      ]
-    }
+      todo: "",
+      list: [{ id: 1, item: "Foo" }, { id: 2, item: "Bar" }]
+    };
   },
 
   created: function() {
-   this.fetchTodoLists();
+    this.fetchTodoLists();
   },
 
   methods: {
-    addTodo(){
-      this.$http.post('todolists.json', { item: this.todo }, {})
-      .then((res) => this.fetchTodoLists(), this.todo = '')
-      .catch((error) => console.log('Got a problem' + error));
+    addTodo() {
+      this.$http
+        .post("todolists.json", { item: this.todo }, {})
+        .then(res => this.fetchTodoLists(), (this.todo = ""))
+        .catch(error => console.log("Got a problem" + error));
     },
 
-    deleteTodo(todo_id){
-      this.$http.delete('todolists/'+ todo_id +'.json')
-      .then((res) => this.fetchTodoLists())
-      .catch((error) => console.log('Got a problem' + error));
+    deleteTodo(todo_id) {
+      this.$http
+        .delete("todolists/" + todo_id + ".json")
+        .then(res => this.fetchTodoLists())
+        .catch(error => console.log("Got a problem" + error));
     },
 
     changeTodo(todoItem) {
-      todoItem.editable = !todoItem.editable
+      todoItem.editable = !todoItem.editable;
     },
 
     updateTodo(todoItem) {
-      this.$http.put('todolists/'+ todoItem.id, { item: todoItem.item }, {})
-      .then((res) => this.fetchTodoLists())
-      .catch((error) => console.log('Got a problem' + error));
+      this.$http
+        .put("todolists/" + todoItem.id, { item: todoItem.item }, {})
+        .then(res => this.fetchTodoLists())
+        .catch(error => console.log("Got a problem" + error));
     },
 
     fetchTodoLists: function() {
-      const resource = this.$resource('/todolists.json/{ id }');
-      resource.get().then(function(response){
+      const resource = this.$resource("/todolists.json/{ id }");
+      resource.get().then(function(response) {
         // this.list = response.data
 
-        const newList = response.data.map((oldItem) => {
-          return { ...oldItem, editable: true }
+        const newList = response.data.map(oldItem => {
+          return { ...oldItem, editable: true };
         });
 
-        this.list = newList
+        this.list = newList;
       });
     }
   }
-}
+};
 </script>
